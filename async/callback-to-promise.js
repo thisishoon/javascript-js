072b18195384d01e26b61b8d86666081b0d7eef7
thisishoon <jihoon522@naver.com>
2020-10-06 16:05:59 +0900

    promise study & replace callback to promise

---
+++ async/callback-to-promise.js
@@ -0,0 +1,37 @@
+class UserStorage{
+    loginUser(id, password){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(
+                    (id === 'thisis' && password === 'hoon') ||
+                    (id === 'this' && password ==='ishoon')
+                ){
+                    resolve(id);
+                } else {
+                    onError(new Error('not found'));
+                }
+            }, 2000);
+        });
+    }
+    getRoles(user){
+        return new Promise((resolve, reject) => {
+            setTimeout(()=>{
+                if(user === 'thisis'){
+                    resolve({name: 'thisis', role: 'admin'});
+                }
+                else{
+                    reject(new Error('no access'));
+                }
+            }, 1000);
+        });
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+
+userStorage.loginUser(id, password)
+.then(userStorage.getRoles)
+.then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
+.catch(alert);
