ff78329a86398381520b9ff98b40def656e63ca8
thisishoon <jihoon522@naver.com>
2020-10-12 17:29:21 +0900

    session을 활용한 데이터 저장. 서버에서 key를 통해 unique한 id를 생성하고 저장하여 관리하기. 클라이언트는 sessionid만으로 서버에게 요청. express-session을 사용하면 쉽게 사용 가능

--- node/session.js
+++ node/session.js
@@ -12,28 +12,32 @@ const parseCookies = (cookie = '') =>
             return acc;
         }, {});
 
+const session = {};
+
 http.createServer(async (req, res) => {
     const cookies = parseCookies(req.headers.cookie);
-    console.log(req.headers.cookie);
-    // 주소가 /login으로 시작하는 경우
     if (req.url.startsWith('/login')) {
         const { query } = url.parse(req.url);
         const { name } = qs.parse(query);
         const expires = new Date();
-        // 쿠키 유효 시간을 현재시간 + 5분으로 설정
         expires.setMinutes(expires.getMinutes() + 5);
+        const uniqueInt = Date.now();
+        session[uniqueInt] = {
+            name,
+            expires,
+        };
         res.writeHead(302, {
             Location: '/',
-            'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            'Set-Cookie': `session=${uniqueInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
         });
         res.end();
-        // name이라는 쿠키가 있는 경우
-    } else if (cookies.name) {
+        // 세션쿠키가 존재하고, 만료 기간이 지나지 않았다면
+    } else if (cookies.session && session[cookies.session].expires > new Date()) {
         res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end(`${cookies.name}님 안녕하세요`);
+        res.end(`${session[cookies.session].name}님 안녕하세요`);
     } else {
         try {
-            const data = await fs.readFile('./cookie.html');
+            const data = await fs.readFile('./cookie2.html');
             res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
             res.end(data);
         } catch (err) {
@@ -42,6 +46,6 @@ http.createServer(async (req, res) => {
         }
     }
 })
-    .listen(8080, () => {
-        console.log('8080번 포트에서 서버 대기 중입니다!');
+    .listen(8085, () => {
+        console.log('8085번 포트에서 서버 대기 중입니다!');
     });
